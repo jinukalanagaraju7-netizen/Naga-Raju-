d5dbcf048d0b649a842cc598fc11c89e92a72c07
jinukalanagaraju7-netizen <jinukalanagaraju7@gmail.com>
2026-03-17 19:06:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
-function predictDisease(){
+function register(){
 
-var selected=[];
-var boxes=document.querySelectorAll("input[name='symptom']:checked");
+var user=document.getElementById("newuser").value;
+var pass=document.getElementById("newpass").value;
 
-boxes.forEach(function(box){
-selected.push(box.value);
-});
-
-if(selected.length < 2){
-document.getElementById("result").innerHTML="Select at least 2 symptoms";
+if(user=="" || pass==""){
+alert("Enter username and password");
 return;
 }
 
-var disease="General Viral Infection";
+localStorage.setItem("username",user);
+localStorage.setItem("password",pass);
 
-if(selected.includes("fever") && selected.includes("cough"))
-disease="Flu";
+alert("Account Created");
 
-else if(selected.includes("fever") && selected.includes("vomiting"))
-disease="Food Poisoning";
+window.location="dashboard.html";
 
-else if(selected.includes("headache") && selected.includes("blurvision"))
-disease="Migraine";
+}
 
-else if(selected.includes("fatigue") && selected.includes("weightloss"))
-disease="Diabetes";
+function login(){
 
-else if(selected.includes("shortbreath") && selected.includes("chestpain"))
-disease="Heart Problem";
+var user=document.getElementById("username").value;
+var pass=document.getElementById("password").value;
 
-else if(selected.includes("itching") && selected.includes("rash"))
-disease="Skin Allergy";
+var savedUser=localStorage.getItem("username");
+var savedPass=localStorage.getItem("password");
 
-else if(selected.includes("cold") && selected.includes("sorethroat"))
-disease="Common Cold";
+if(user===savedUser && pass===savedPass){
 
-else if(selected.includes("dizziness") && selected.includes("fatigue"))
-disease="Low Blood Pressure";
+window.location="dashboard.html";
 
-else if(selected.includes("diarrhea") && selected.includes("stomach"))
-disease="Stomach Infection";
+}
+else{
 
-else if(selected.includes("backpain") && selected.includes("legpain"))
-disease="Muscle Pain";
+alert("Wrong Username or Password");
 
-document.getElementById("result").innerHTML="🩺 Possible Disease: <b>"+disease+"</b>";
+}
 
 }
